9beb30d398fb8e202c91304ad3e70e97ae521d3d
benaor <benjamingirard25@gmail.com>
2020-04-25 22:59:29 +0200

    set interval 1s

--- script.js
+++ script.js
@@ -9,25 +9,30 @@ document.getElementById('date').textContent = date;
 //URL for API
 const url = 'https://blockchain.info/ticker';
 
-//created a query
-let requete = new XMLHttpRequest();
-requete.open('GET',url); //first param is GET/POST, second is URL
-requete.responseType = 'json'; //We want response in format Json
-requete.send(); // We send the query
+function recoverPrice() {
 
-//as soon as we receive a response, we execute the function
-requete.onload = function() {
-    if(requete.readyState === XMLHttpRequest.DONE){
+    //created a query
+    let requete = new XMLHttpRequest();
+    requete.open('GET', url); //first param is GET/POST, second is URL
+    requete.responseType = 'json'; //We want response in format Json
+    requete.send(); // We send the query
 
-        if(requete.status === 200){
+    //as soon as we receive a response, we execute the function
+    requete.onload = function () {
+        if (requete.readyState === XMLHttpRequest.DONE) {
 
-            let response     = requete.response //stock the response in let
-            let priceInEuros = response.EUR.last; //stock price In euros in let
-            console.log(priceInEuros);
-            document.querySelector('#price_label').textContent = priceInEuros;
-            
-        } else {
-            alert('une erreur est survenu. Veuillez réessayer plus tard');
+            if (requete.status === 200) {
+
+                let response = requete.response //stock the response in let
+                let priceInEuros = response.EUR.last; //stock price In euros in let
+                document.querySelector('#price_label').textContent = priceInEuros;
+
+            } else {
+                alert('une erreur est survenu. Veuillez réessayer plus tard');
+            }
         }
     }
+
 }
+
+setInterval(recoverPrice, 1000);
