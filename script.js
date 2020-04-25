a4bd0e9f2eb93816585a8b7fced680990be0e942
benaor <benjamingirard25@gmail.com>
2020-04-25 22:48:02 +0200

    query to API

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ date = date.toLocaleString('fr-FR');
 
 //insert date
 document.getElementById('date').textContent = date;
+
+
+//URL for API
+const url = 'https://blockchain.info/ticker';
+
+//created a query
+let requete = new XMLHttpRequest();
+requete.open('GET',url); //first param is GET/POST, second is URL
+requete.responseType = 'json'; //We want response in format Json
+requete.send(); // We send the query
+
+//as soon as we receive a response, we execute the function
+requete.onload = function() {
+    if(requete.readyState === XMLHttpRequest.DONE){
+
+    }
+}
